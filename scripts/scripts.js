18a6cbc362320d830e183bd14bbac6106c107681
Mariannebstl <75022693+Mariannebstl@users.noreply.github.com>
2025-02-07 21:59:09 +0100

    9

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,14 @@ import { PNJfiche } from "./PNJ-fiche.js";
 
 Hooks.once("init", async function() {
 
+  console.log("⚡Initialisation du système...");
+
+  // Chargez le contenu du fichier secondary-skills.html
+  const response = await fetch('/systems/swbd/templates/sheets/parts/secondary-skills.html');
+  const secondarySkillsHTML = await response.text();
+  // Enregistrez le partial sous le nom "secondary-skills"
+  Handlebars.registerPartial("secondary-skills", secondarySkillsHTML);
+
      // Désenregistre les fiches d'Actors de base
     Actors.unregisterSheet("core", ActorSheet);
     
